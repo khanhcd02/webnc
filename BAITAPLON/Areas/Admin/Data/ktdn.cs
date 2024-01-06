528dc52530a0eb0c7a9c8a4d67498b562484c8fc
khanhcd02 <phamkhanh20022002@gmail.com>
2024-01-07 00:47:33 +0700

    abc

---
+++ BAITAPLON/Areas/Admin/Data/ktdn.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BAITAPLON.Areas.Admin.Data;
+namespace BAITAPLON.Areas.Admin.Data
+{
+    public class ktdn
+    {
+        public static int dn = 0;
+    }
+}
